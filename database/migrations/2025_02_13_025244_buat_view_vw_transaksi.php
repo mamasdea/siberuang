a720842eb91553c77eb4bf35cda377963f6ebc12
mamasdea <deaaldy.28@gmail.com>
2025-02-13 16:13:22 +0700

    edit vw transaksi

--- database/migrations/2025_02_13_025244_buat_view_vw_transaksi.php
+++ database/migrations/2025_02_13_025244_buat_view_vw_transaksi.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         DB::statement("
         CREATE VIEW vw_transaksi as
-        SELECT 0 AS id, tanggal,no_bukti,'' AS rekening,uraian,nominal AS debet, '' AS kredit FROM uang_giros
+       SELECT '' AS id, tanggal,no_bukti,'' AS rekening,uraian,nominal AS debet, '' AS kredit FROM uang_giros
         UNION ALL SELECT belanjas.id, tanggal,no_bukti,kode_belanja as rekening, uraian, '' AS debet, nilai AS kredit FROM belanjas
-        JOIN rkas ON rkas.id = belanjas.rka_id
+        JOIN rkas ON rkas.id = belanjas.rka_id WHERE belanjas.is_transfer = 1
             ");
     }
 
