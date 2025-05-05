39bb9f7fe92ca0b1cdeb3e7a25c04d3e5a5d4f4e
mamasdea <deaaldy.28@gmail.com>
2025-05-05 15:08:07 +0700

    tailah

--- app/Livewire/Belanja/Ls/BelanjaLs.php
+++ app/Livewire/Belanja/Ls/BelanjaLs.php
@@ -123,7 +123,7 @@ class BelanjaLs extends Component
         // $formattedNoBukti = str_pad($newNoBukti, 4, '0', STR_PAD_LEFT);
 
         $validatedData = $this->validate([
-            'no_bukti'         => 'required|unique:belanja_ls, no_bukti',
+            'no_bukti'         => 'required|unique:belanja_ls,no_bukti',
             'tanggal'         => 'required|date',
             'uraian'          => 'required',
             'sub_kegiatan_id' => 'required|exists:sub_kegiatans,id',
